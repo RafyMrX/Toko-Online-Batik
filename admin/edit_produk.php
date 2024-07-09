3bedf6266a766f2418be3ef0e74776ae0fb9b4e9
ygaafrnsya <yogaafriansya55@gmail.com>
2024-07-09 15:09:27 +0700

    Update edit_produk.php

--- admin/edit_produk.php
+++ admin/edit_produk.php
@@ -7,116 +7,78 @@ $data = mysqli_fetch_assoc($kode);
 
 ?>
 
-
 <div class="container">
-	<h2 style=" width: 100%; border-bottom: 4px solid gray"><b>Edit Produk</b></h2>
+    <h2 style=" width: 100%; border-bottom: 4px solid gray"><b>Edit Produk</b></h2>
 
-	<form action="proses/edit_produk.php" method="POST" enctype="multipart/form-data">
+    <form action="proses/edit_produk.php" method="POST" enctype="multipart/form-data">
 
-		<div class="form-group">
-			<label for="exampleInputFile"><img src="../image/produk/<?= $data['image']; ?>" width="100"></label>
-			<input type="file" id="exampleInputFile" name="files">
-			<p class="help-block">Pilih Gambar untuk Produk</p>
-		</div>
+        <div class="form-group">
+            <label for="exampleInputFile"><img src="../image/produk/<?= $data['image']; ?>" width="100"></label>
+            <input type="file" id="exampleInputFile" name="files">
+            <p class="help-block">Pilih Gambar untuk Produk</p>
+        </div>
 
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputEmail1">Kode Produk</label>
-					<input type="text" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Nama Produk" disabled value="<?= $data['kode_produk']; ?>">
-					<input type="hidden" name="kode" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Nama Produk"  value="<?= $data['kode_produk']; ?>">
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputEmail1">Nama Produk</label>
-					<input type="text" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Nama Produk" name="nama" value="<?= $data['nama']; ?>">
-				</div>
-			</div>
-		</div>
-<?php 
-$arr = explode(",", $data['ukuran']);
-$arr2 = explode(",", $data['harga']);
-$jml = count($arr);
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Kode Produk</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Nama Produk" disabled value="<?= $data['kode_produk']; ?>">
+                    <input type="hidden" name="kode" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Nama Produk"  value="<?= $data['kode_produk']; ?>">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Nama Produk</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Nama Produk" name="nama" value="<?= $data['nama']; ?>">
+                </div>
+            </div>
+        </div>
 
-for ($a=0; $a < $jml ; $a++) { 
- ?>
-	<div class="row">
-			<div class="col-md-6">
-				<div class="form-group form-check">
-					<label for="exampleInputEmail1">Ukuran</label><br>
-					<select name="ukuran[]" class="form-control">
-					<?php for ($i=0; $i < $jml ; $i++) { 
-						if ($arr[$a] == $arr[$i]) {
-							$select = "selected";
-						}
-						else{
-							$select = "";
-						}
-					 ?>
-						<option <?php echo $select; ?> value="<?php echo $arr[$i] ?>"><?php echo strtoupper($arr[$i]); ?></option>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Ukuran</label><br>
+                    <select name="ukuran[]" class="form-control">
+                        <option value="all-size" <?php if ($data['ukuran'] == 'all-size') echo 'selected'; ?>>All Size</option>
+                    </select>
+                </div>
+            </div>
 
-					<?php } ?>
-					</select>
-				</div>
-			</div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Harga</label>
+                    <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Contoh : 12000" name="harga[]" value="<?= $data['harga']; ?>">
+                    <p class="help-block">Isi Harga tanpa menggunakan Titik(.) atau Koma (,)</p>
+                </div>
+            </div>
+        </div>
 
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputEmail1">Harga</label>
-					<input type="number" class="form-control" id="exampleInputEmail1" placeholder="Contoh : 12000" name="harga[]" value="<?php echo $arr2[$a]; ?>">
-					<p class="help-block">Isi Harga tanpa menggunakan Titik(.) atau Koma (,)</p>
-				</div>
-			</div>
-		</div>
-<?php 
-	}
- ?>
-<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputEmail1">Berat (Gram)</label>
-					<input type="text" class="form-control" id="exampleInputEmail1" placeholder="Contoh : Masukkan dalam satuan Gram" name="berat" value="<?php echo $data['berat']; ?>">					
-				</div>
-			</div>
-		</div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Berat (Gram)</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Contoh : Masukkan dalam satuan Gram" name="berat" value="<?php echo $data['berat']; ?>">                    
+                </div>
+            </div>
+        </div>
 
-		<div class="form-group">
-			<label for="exampleInputPassword1">Deskripsi</label>
-			<textarea name="desk" class="form-control">
-				<?= $data['deskripsi']; ?>
-			</textarea>
-		</div>
-		
-		<div class="row">
-			
-			<div class="col-md-6">
-				<button type="submit"  class="btn btn-warning btn-block" ><i class="glyphicon glyphicon-edit"></i> Edit</button>
-			</div>	
-			<div class="col-md-6">
-				<a href="m_produk.php" class="btn btn-danger btn-block">Cancel</a>
-			</div>
-		</div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Deskripsi</label>
+            <textarea name="desk" class="form-control"><?= $data['deskripsi']; ?></textarea>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-6">
+                <button type="submit" class="btn btn-warning btn-block"><i class="glyphicon glyphicon-edit"></i> Edit</button>
+            </div>  
+            <div class="col-md-6">
+                <a href="m_produk.php" class="btn btn-danger btn-block">Cancel</a>
+            </div>
+        </div>
 
-		<br>
-
-</form>
-	</div>
-
-
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+        <br>
+    </form>
+</div>
 
 <?php 
 include 'footer.php';

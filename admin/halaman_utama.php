4aa910089f8f82a86481393f8f65f04ef3affb31
Farid123456-spk <faridnurdiansyah45@smk.belajar.id>
2024-07-14 13:51:29 +0700

    update laporan omset

--- admin/halaman_utama.php
+++ admin/halaman_utama.php
@@ -1,6 +1,6 @@
 <?php 
-
 include 'header.php';
+
 // pesanan baru 
 $result1 = mysqli_query($conn, "SELECT distinct invoice FROM produksi WHERE terima = 0 and tolak = 0");
 $jml1 = mysqli_num_rows($result1);
@@ -12,53 +12,57 @@ $jml2 = mysqli_num_rows($result2);
 // pesanan diterima
 $result3 = mysqli_query($conn, "SELECT distinct invoice FROM produksi WHERE  terima = 1");
 $jml3 = mysqli_num_rows($result3);
-
 ?>
-<div class="container">
-	<div class="row">
-		<div class="col-md-4" >
-			<div style="background-color: #87CEFA; padding-bottom: 60px; padding-left: 20px;padding-right: 20px; padding-top: 10px;">
-				<h4>PESANAN BARU</h4>
-				<h4 style="font-size: 56pt;"><b><?= $jml1; ?></b></h4>
-			</div>
-		</div>
 
-		<div class="col-md-4" >
-			<div style="background-color: #FF0000; padding-bottom: 60px; padding-left: 20px;padding-right: 20px; padding-top: 10px;">
-				<h4>PESANAN DIBATALKAN</h4>
-				<h4 style="font-size: 56pt;"><b><?= $jml2; ?></b></h4>
-			</div>
-		</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <style type="text/css">
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            margin: 0;
+            background-color: #FFB347			; /* Warna emas pastel */
+        }
+        .container {
+            flex: 1;
+        }
+        @media print {
+            .print {
+                display: none;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div style="background-color: #87CEFA; padding-bottom: 60px; padding-left: 20px; padding-right: 20px; padding-top: 10px;">
+                    <h4>PESANAN BARU</h4>
+                    <h4 style="font-size: 56pt;"><b><?= htmlspecialchars($jml1); ?></b></h4>
+                </div>
+            </div>
 
-		<div class="col-md-4" >
-			<div style="background-color: #00FF7F; padding-bottom: 60px; padding-left: 20px;padding-right: 20px; padding-top: 10px;">
-				<h4>PESANAN DITERIMA</h4>
-				<h4 style="font-size: 56pt;"><b><?= $jml3; ?></b></h4>
-			</div>
-		</div>
+            <div class="col-md-4">
+                <div style="background-color: #FF0000; padding-bottom: 60px; padding-left: 20px; padding-right: 20px; padding-top: 10px;">
+                    <h4>PESANAN DIBATALKAN</h4>
+                    <h4 style="font-size: 56pt;"><b><?= htmlspecialchars($jml2); ?></b></h4>
+                </div>
+            </div>
 
-	</div>
-</div>
+            <div class="col-md-4">
+                <div style="background-color: #00FF7F; padding-bottom: 60px; padding-left: 20px; padding-right: 20px; padding-top: 10px;">
+                    <h4>PESANAN DITERIMA</h4>
+                    <h4 style="font-size: 56pt;"><b><?= htmlspecialchars($jml3); ?></b></h4>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
-<?php 
-include 'footer.php';
-?>
+    <?php include 'footer.php'; ?>
+</body>
+</html>

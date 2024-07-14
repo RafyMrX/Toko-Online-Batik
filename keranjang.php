b490e27d23054c3e6a53df6b49feb42720a29586
Valreand <haikalfauzi0408@gmail.com>
2024-07-14 10:42:28 +0700

    Update keranjang.php

--- keranjang.php
+++ keranjang.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 include 'header.php';
 
-if(isset($_POST['submit1'])){
+if (isset($_POST['submit1'])) {
     $id_keranjang = $_POST['id'];
     $qty = $_POST['qty'];
 
     $edit = mysqli_query($conn, "UPDATE keranjang SET qty = '$qty' WHERE id_keranjang = '$id_keranjang'");
-    if($edit){
+    if ($edit) {
         echo "
         <script>
         alert('KERANJANG BERHASIL DIPERBARUI');
@@ -14,10 +14,10 @@ if(isset($_POST['submit1'])){
         </script>
         ";
     }
-} else if(isset($_GET['del'])){
+} else if (isset($_GET['del'])) {
     $id_keranjang = $_GET['id'];
     $del = mysqli_query($conn, "DELETE FROM keranjang WHERE id_keranjang = '$id_keranjang'");
-    if($del){
+    if ($del) {
         echo "
         <script>
         alert('1 PRODUK DIHAPUS');
@@ -31,67 +31,67 @@ if(isset($_POST['submit1'])){
 <div class="container" style="padding-bottom: 300px;">
     <h2 style="width: 100%; border-bottom: 4px solid #ff8680"><b>Keranjang</b></h2>
     <table class="table table-striped">
-        <?php 
-        if(isset($_SESSION['user'])){
+        <?php
+        if (isset($_SESSION['user'])) {
             $kode_cs = $_SESSION['kd_cs'];
             // CEK JUMLAH KERANJANG
             $cek = mysqli_query($conn, "SELECT * FROM keranjang WHERE kode_customer = '$kode_cs'");
             $jml = mysqli_num_rows($cek);
 
-            if($jml > 0){
-        ?>  
-               <thead>
-                   <tr>
-                      <th scope="col">No</th>
-                      <th scope="col">Image</th>
-                      <th scope="col">Nama</th>
-                      <th scope="col">Harga</th>
-                      <th scope="col">Qty</th>
-                      <th scope="col">Ukuran</th>
-                      <th scope="col">SubTotal</th>
-                      <th scope="col">Action</th>
-                   </tr>
-               </thead>
-               <tbody>
-                   <?php 
-                   $result = mysqli_query($conn, "SELECT k.id_keranjang as keranjang, k.kode_produk as kd, k.nama_produk as nama, k.qty as jml, p.image as gambar, p.harga as hrg, k.ukuran as ukuran 
-                                               FROM keranjang k 
-                                               JOIN produk p ON k.kode_produk = p.kode_produk 
-                                               WHERE k.kode_customer = '$kode_cs'");
-                   $no = 1;
-                   $hasil = 0;
-                   while($row = mysqli_fetch_assoc($result)){
-                       $harga = floatval($row['hrg']);
-                       $jumlah = intval($row['jml']);
-                       $subtotal = $harga * $jumlah;
-                       $hasil += $subtotal;
+            if ($jml > 0) {
+        ?>
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Harga</th>
+                        <th scope="col">Qty</th>
+                        <th scope="col">Ukuran</th>
+                        <th scope="col">SubTotal</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $result = mysqli_query($conn, "SELECT k.id_keranjang as keranjang, k.kode_produk as kd, k.nama_produk as nama, k.qty as jml, p.image as gambar, k.harga as hrg, k.ukuran as ukuran 
+                                                   FROM keranjang k 
+                                                   JOIN produk p ON k.kode_produk = p.kode_produk 
+                                                   WHERE k.kode_customer = '$kode_cs'");
+                    $no = 1;
+                    $hasil = 0;
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $harga = floatval($row['hrg']);
+                        $jumlah = intval($row['jml']);
+                        $subtotal = $harga * $jumlah;
+                        $hasil += $subtotal;
                     ?>
-                       <tr>
-                           <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                               <input type="hidden" name="id" value="<?php echo $row['keranjang']; ?>">
-                               <td scope="row"><?= $no; ?></td>
-                               <td><img src="image/produk/<?= $row['gambar']; ?>" width="100"></td>
-                               <td><?= $row['nama']; ?></td>
-                               <td>Rp.<?= number_format($harga); ?></td>
-                               <td><input type="number" name="qty" class="form-control" style="text-align: center;" value="<?= $jumlah; ?>"></td>
-                               <td><?= $ukuran; ?></td>
-                               <td>Rp.<?= number_format($subtotal); ?></td>
-                               <td>
-                                   <button type="submit" name="submit1" class="btn btn-warning">Update</button> 
-                                   | 
-                                   <a href="keranjang.php?del=1&id=<?= $row['keranjang']; ?>" class="btn btn-danger" onclick="return confirm('Yakin ingin dihapus ?')">Delete</a>
-                               </td>
-                         </form>
-                       </tr>
-                   <?php 
-                       $no++;
+                        <tr>
+                            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $row['keranjang']; ?>">
+                                <td scope="row"><?= $no; ?></td>
+                                <td><img src="image/produk/<?= $row['gambar']; ?>" width="100"></td>
+                                <td><?= $row['nama']; ?></td>
+                                <td>Rp.<?= number_format($harga); ?></td>
+                                <td><input type="number" name="qty" class="form-control" style="text-align: center;" value="<?= $jumlah; ?>"></td>
+                                <td><?= strtoupper($row['ukuran']); ?></td>
+                                <td>Rp.<?= number_format($subtotal); ?></td>
+                                <td>
+                                    <button type="submit" name="submit1" class="btn btn-warning">Update</button>
+                                    |
+                                    <a href="keranjang.php?del=1&id=<?= $row['keranjang']; ?>" class="btn btn-danger" onclick="return confirm('Yakin ingin dihapus ?')">Delete</a>
+                                </td>
+                            </form>
+                        </tr>
+                    <?php
+                        $no++;
                     }
                     ?>
                     <tr>
                         <td colspan="8" style="text-align: right; font-weight: bold;">Grand Total = Rp.<?= number_format($hasil); ?></td>
-                   </tr>
-               </tbody>  
-        <?php 
+                    </tr>
+                </tbody>
+        <?php
             } else {
                 echo "
                 <tr>
@@ -125,11 +125,11 @@ if(isset($_POST['submit1'])){
             <?php if ($jml > 0) { ?>
                 <a href="index.php" class="btn btn-success">Lanjutkan Belanja</a>
                 <a href="checkout.php?kode_cs=<?= $kode_cs; ?>" class="btn btn-primary">Checkout</a>
-           <?php } ?>
+            <?php } ?>
         </div>
     </div>
 </div>
 
-<?php 
+<?php
 include 'footer.php';
 ?>
